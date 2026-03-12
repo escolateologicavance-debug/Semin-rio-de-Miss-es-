3f61d6df2a2f015ab09b2f6e4c26ad877ef57897
Gilberto Moreira <escolateologicavance@gmail.com>
2026-03-12 10:03:40 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'missoes-v1';
+const assets = [
+  '/',
+  'index.html',
+  '1.html',
+  '2.html',
+  '3.html',
+  '4.html',
+  '5.html',
+  '6.html',
+  'logo-192.png',
+  'logo-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
